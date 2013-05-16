157d1418229ee69bd79867234efe7896ef625f6f
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2013-05-16 14:34:58 -0300

    solving the issue with delete *.pdb files replacing the array ['*\\.svn\\*', '*uploads', '*Test.dll', '*Test.dll.config', '*Tests.dll', '*Tests.dll.config', '*Test.exe', '*_accessor.dll' '*.pdb', '*.nlp'] to ['*\\.svn\\*', '*uploads', '*Test.dll', '*Test.dll.config', '*Tests.dll', '*Tests.dll.config', '*Test.exe', '*_accessor.dll', '*.pdb', '*.nlp']

--- pyhammer/filters/csbinaryfilefilter.py
+++ pyhammer/filters/csbinaryfilefilter.py
@@ -3,4 +3,4 @@ from pyhammer.filters.filefilter import FileFilter
 class CsBinaryFileFilter(FileFilter):
     def __init__( self ):
         FileFilter.__init__( self, exclude = ['*\\.svn\\*', '*uploads', '*Test.dll', '*Test.dll.config', '*Tests.dll',
-                                              '*Tests.dll.config', '*Test.exe', '*_accessor.dll' '*.pdb', '*.nlp'])
+                                              '*Tests.dll.config', '*Test.exe', '*_accessor.dll', '*.pdb', '*.nlp'])
