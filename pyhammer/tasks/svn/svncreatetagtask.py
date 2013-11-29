339b2a874222ddcd80e2b53339b45629084caf7e
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2013-11-29 11:45:56 -0200

    Added VersionNumber parameter in SvnCreateTagTask in case of not necessary of versionFile

--- pyhammer/tasks/svn/svncreatetagtask.py
+++ pyhammer/tasks/svn/svncreatetagtask.py
@@ -1,41 +1,66 @@
 # -*- coding: utf-8 -*-
+import codecs
 import re
 from pyhammer.tasks.taskbase import TaskBase
 from pyhammer.utils import execProg
+
 
 class SvnCreateTagTask(TaskBase):
     __dirTrunk = ""
     __dirTag = ""
     __versionFile = ""
 
-    def __init__( self, dirTrunk, dirTag, versionFile = None, includeRevision = False, encoding = "ISO-8859-1" ):
+    def __init__(self, dirTrunk, dirTag, versionFile=None, includeRevision=False, versionNumber=None, encoding=None):
         super(SvnCreateTagTask, self).__init__()
-        
+
         self.__dirTrunk = dirTrunk
         self.__dirTag = dirTag
         self.__versionFile = versionFile
         self.__encoding = encoding
         self.__includeRevision = includeRevision
+        self.__versionNumber = versionNumber
 
-    def do( self ):
+    def do(self):
+        versionNumber = ''
         if self.__versionFile is not None:
-            f = open(self.__versionFile, 'r', encoding=self.__encoding)
-            content = f.read()
-            f.close()
+            f = None
+            content = None
+            try:
+                if not self.__encoding is None:
+                    f = codecs.open(self.__versionFile, 'r', encoding=self.__encoding)
+                else:
+                    f = open(self.__versionFile, 'r')
+
+                content = f.read()
+
+            except IOError as e:
+                self.reporter.failure("Can not read file: %s\nException: %s" % self.__versionFile, e.message)
+                return False
+            finally:
+                if f is not None:
+                    f.close()
+
             version = None
-            if self.__includeRevision is False:
-                version = re.search( '[\'\"](\d+)\.(\d+)\.(\d+)', content )
+            versionNumber = None
+            if self.__includeRevision:
+                version = re.search('(\d+)\.(\d+)\.(\d+)\.(\d+)', content)
+                versionNumber = version.group(1) + '.' + version.group(2) + '.' + version.group(3) + '.' + version.group(4)
             else:
-                version = re.search( '[\'\"](\d+)\.(\d+)\.(\d+)\.(\d+)', content )
+                version = re.search('(\d+)\.(\d+)\.(\d+)', content)
+                versionNumber = version.group(1) + '.' + version.group(2) + '.' + version.group(3)
+        elif self.__versionNumber is not None:
+            versionNumber = self.__versionNumber
+        else:
+            self.reporter.failure("Version file or Versio Number is None, please give a path to search version.")
+            return False
 
-            versionNumber = version.group(1) + '.' + version.group(2) + '.' + version.group(3)
-            self.__dirTag = self.__dirTag + '/' + versionNumber
+        self.__dirTag = self.__dirTag + '/' + versionNumber
 
-        self.reporter.message( "TRUNK DIR: %s" % self.__dirTrunk )
-        self.reporter.message( "TAG DIR: %s" % self.__dirTag )
+        self.reporter.message("TRUNK DIR: %s" % self.__dirTrunk)
+        self.reporter.message("TAG DIR: %s" % self.__dirTag)
         commitMessage = "Created by Build"
-        command = 'svn copy --non-interactive --trust-server-cert "' + self.__dirTrunk + '" "' + self.__dirTag + '" -m \"'+commitMessage+'\"'
+        command = 'svn copy --non-interactive --trust-server-cert "' + self.__dirTrunk + '" "' + self.__dirTag + '" -m \"' + commitMessage + '\"'
 
-        self.reporter.message( "COMMAND: %s" % command )
-        
-        return execProg( command, self.reporter ) == 0
+        self.reporter.message("COMMAND: %s" % command)
+
+        return execProg(command, self.reporter) == 0
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyhammer',
-    version='0.3.25',
+    version='0.3.28',
     license='MIT',
     description='Build automation library',
     author='Afonso Franca',
