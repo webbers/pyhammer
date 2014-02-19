27bf8372abe5ebf02a42572794f0bac86201f671
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-02-19 16:19:30 -0300

    Fixing issues.

--- pyhammer/filters/jarfilefilter.py
+++ pyhammer/filters/jarfilefilter.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from pyhammer.filters.filefilter import FileFilter
 
-class CsBinaryFileFilter(FileFilter):
+class JarFileFilter(FileFilter):
     def __init__( self ):
-        FileFilter.__init__( self, exclude = ['*\\.svn\\*', '*uploads', '*Test.dll', '*Test.dll.config', '*Tests.dll',
-                                              '*Tests.dll.config', '*Test.exe', '*_accessor.dll', '*.pdb', '*.nlp'])
+        FileFilter.__init__( self, include = ['*.jar'])
