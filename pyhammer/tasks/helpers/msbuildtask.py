9039f6a2d46087322ffabda391bb160e0bdd8efc
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-03-19 11:54:34 -0300

    Added VisualStudioVersion parameter to msbuildtask.py

--- pyhammer/tasks/helpers/msbuildtask.py
+++ pyhammer/tasks/helpers/msbuildtask.py
@@ -5,11 +5,11 @@ from pyhammer.utils import execProg
 class MsBuildTask(TaskBase):
     """Cs Project Build Step"""
 
-    def __init__( self, csProjectPath, outputDir, target = "Rebuild", mode = "Release", frameworkVersion = "v4.0.30319" ):
+    def __init__( self, csProjectPath, outputDir, target = "Rebuild", mode = "Release", visualStudioVersion = "12.0" ):
         super(MsBuildTask, self).__init__()
         
         msBuildPath = "msbuild.exe"
-        self.command = "\"%s\" \"%s\" /t:%s /p:Configuration=%s;OutDir=\"%s\"\\ " % ( msBuildPath, csProjectPath, target, mode, outputDir )
+        self.command = "\"%s\" \"%s\" /t:%s /p:Configuration=%s;VisualStudioVersion=%s;OutDir=\"%s\"\\ " % ( msBuildPath, csProjectPath, target, mode, visualStudioVersion, outputDir )
         self.csProjectPath = csProjectPath
 
     def do( self ):
