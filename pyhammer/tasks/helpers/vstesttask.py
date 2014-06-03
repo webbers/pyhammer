f64d21c616257ab7f3d8c8742c98038178106f56
Renan Borges <renanborges02@gmail.com>
2014-06-03 11:30:01 -0300

    Testing adding a new Task VsTestTask

--- pyhammer/tasks/helpers/vstesttask.py
+++ pyhammer/tasks/helpers/vstesttask.py
@@ -7,10 +7,10 @@ class VsTestTask(TaskBase):
     """Cs Project Build Step"""
 
     def __init__( self, csProjectPath, outputDir, target = "Rebuild", mode = "Release", visualStudioVersion = "12.0" ):
-        super(MsBuildTask, self).__init__()
+        super(VsTestTask, self).__init__()
         
-        msBuildPath = "vstest.console.exe"
-        self.command = "\"%s\" \"%s\" /t:%s /property:Configuration=%s;VisualStudioVersion=%s;OutDir=\"%s\"\\ " % ( msBuildPath, csProjectPath, target, mode, visualStudioVersion, outputDir )
+        vsTestPath = "vstest.console.exe"
+        self.command = "\"%s\" \"%s\" /t:%s /property:Configuration=%s;VisualStudioVersion=%s;OutDir=\"%s\"\\ " % ( vsTestPath, csProjectPath, target, mode, visualStudioVersion, outputDir )
         self.csProjectPath = csProjectPath
         self.workingDir = os.path.dirname(csProjectPath)
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyhammer',
-    version='0.3.41',
+    version='0.3.42',
     license='MIT',
     description='Build automation library',
     author='Afonso Franca',
