1205bdac9374dff38bf78c1661e5d826663ef184
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-09-03 15:30:48 -0300

    Bugfix.

--- pyhammer/tasks/helpers/msbuildtask.py
+++ pyhammer/tasks/helpers/msbuildtask.py
@@ -6,15 +6,20 @@ from pyhammer.utils import execProg
 class MsBuildTask(TaskBase):
     """Cs Project Build Step"""
 
-    def __init__( self, csProjectPath, outputDir = '', target = "Rebuild", mode = "Release", visualStudioVersion = "12.0" ):
+    def __init__(self, csProjectPath, outputDir='', target="Rebuild", mode="Release", visualStudioVersion="12.0"):
         super(MsBuildTask, self).__init__()
         
         msBuildPath = "msbuild.exe"
-        self.command = "\"%s\" \"%s\" /t:%s /property:Configuration=%s;VisualStudioVersion=%s;OutDir=\"%s\"\\ " % ( msBuildPath, csProjectPath, target, mode, visualStudioVersion, outputDir )
+
+        self.command = "\"%s\" \"%s\" /t:%s /property:Configuration=%s;VisualStudioVersion=%s " % \
+                       (msBuildPath, csProjectPath, target, mode, visualStudioVersion)
+
+        if outputDir is not None and outputDir != '':
+            self.command = '%s;OutDir=\"%s\"' % (self.command, outputDir)
+
         self.csProjectPath = csProjectPath
         self.workingDir = os.path.dirname(csProjectPath)
 
-    def do( self ):
-        self.reporter.message( "BUILD CS PROJECT: %s" % self.csProjectPath )
+    def do(self):
+        self.reporter.message("BUILD CS PROJECT: %s" % self.csProjectPath)
         return execProg( self.command, self.reporter, self.workingDir ) == 0
-    
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyhammer',
-    version='0.5.2',
+    version='0.5.3',
     license='MIT',
     description='Build automation library',
     author='Afonso Franca',
