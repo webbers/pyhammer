ad62ea031e1307c28c139b2e2b012294d37f9110
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-09-03 15:16:39 -0300

    Fixing version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyhammer',
-    version='0.3.57',
+    version='0.5.2',
     license='MIT',
     description='Build automation library',
     author='Afonso Franca',
