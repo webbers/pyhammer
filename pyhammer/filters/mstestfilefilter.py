6353f7f04b9b365460326e48e67938dbb254753c
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-09-08 15:00:45 -0300

    Added exclude files to MsTestFileFilter

--- pyhammer/filters/mstestfilefilter.py
+++ pyhammer/filters/mstestfilefilter.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from pyhammer.filters.filefilter import FileFilter
 
+
 class MsTestFileFilter(FileFilter):
-    def __init__( self, mode = 'Release'):
-        if mode == None:
+    def __init__(self, mode='Release', exclude=[]):
+        if mode is None:
             mode = 'Release'
-        FileFilter.__init__( self, include=['*Tests.dll'], folderPattern='*\\bin\\'+mode+'\\*')
+        FileFilter.__init__(self, include=['*Tests.dll'], folderPattern='*\\bin\\'+mode+'\\*', exclude=exclude)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyhammer',
-    version='0.5.4',
+    version='0.5.5',
     license='MIT',
     description='Build automation library',
     author='Afonso Franca',
