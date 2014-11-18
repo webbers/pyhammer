f01f4d265e7c65d6a4d5db46037f8cd145978a93
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2014-11-18 11:33:08 -0200

    Bugfix

--- pyhammer/tasks/helpers/mstesttask.py
+++ pyhammer/tasks/helpers/mstesttask.py
@@ -9,24 +9,38 @@ class MsTestTask(TaskBase):
 
     def __init__( self, csTestDllPath = "", testSettingsPath = "", baseDir = "", buildMode = "", exclude = []):
         super(MsTestTask, self).__init__()
-        self.command = []
 
+        self.__csTestDllPath = csTestDllPath
+        self.__testSettingsPath = testSettingsPath
+        self.__baseDir = baseDir
+        self.__buildMode = buildMode
+        self.__exclude = exclude
+
+    def do( self ):
+        command = []
         testSettingsCommand = ""
-        if testSettingsPath != "":
-            testSettingsCommand = "/testsettings:\"%s\"" % testSettingsPath
+        if self.__testSettingsPath != "":
+            testSettingsCommand = "/testsettings:\"%s\"" % self.__testSettingsPath
 
-        if len(csTestDllPath) == 0 and len(baseDir) != 0:
-            csTestDllPath = MsTestFileFilter(buildMode, exclude).Filter(baseDir)
+        if len(self.__csTestDllPath) == 0 and len(self.__baseDir) != 0:
+            self.__csTestDllPath = MsTestFileFilter(self.__buildMode, self.__exclude).Filter(self.__baseDir)
 
-        if type(csTestDllPath) == str:
-            self.command.append(["""MSTest.exe /testcontainer:\"%s\" %s""" % ( csTestDllPath, testSettingsCommand ), csTestDllPath])
+        if type(self.__csTestDllPath) == str:
+            command.append(["""MSTest.exe /testcontainer:\"%s\" %s""" % (self.__csTestDllPath, testSettingsCommand), self.__csTestDllPath])
         else:
-            files = [file for file in csTestDllPath if os.path.splitext(file)[1] == '.dll']
+            files = [file for file in self.__csTestDllPath if os.path.splitext(file)[1] == '.dll']
 
             for dllTestPath in files:
-                self.command.append(["""MSTest.exe /testcontainer:\"%s\" %s""" % ( dllTestPath, testSettingsCommand ), dllTestPath])
+                command.append(["""MSTest.exe /testcontainer:\"%s\" %s""" % (dllTestPath, testSettingsCommand), dllTestPath])
 
-        self.csTestDllPath = csTestDllPath
+        result = True
+        for comm in command:
+            self.reporter.message("RUN CS UNITTEST: %s" % comm[1])
+            print(comm)
+            result = execProg(comm[0], self.reporter, os.path.dirname(comm[1])) == 0
+            if not result:
+                break;
+        return result
 
     def _isValidTest(self, execResult):
         result = True
@@ -34,14 +48,4 @@ class MsTestTask(TaskBase):
             if execItem == False:
                 result = False
 
-        return result
-
-    def do( self ):
-        result = True
-        for comm in self.command:
-            self.reporter.message( "RUN CS UNITTEST: %s" % comm[1] )
-            print(comm)
-            result = execProg(comm[0], self.reporter, os.path.dirname(comm[1])) == 0
-            if not result:
-                break;
         return result
